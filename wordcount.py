afa38df56c21fa14b28c6cca8452e6bc48ac9e7e
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 15:46:55 -0700

    Finishing up Ex 07

--- wordcount.py
+++ wordcount.py
@@ -10,14 +10,43 @@ for line in filename:
 
     # check if words are already in dictionary, increment by 1
     for word in words:
-        word = word.lower()
-        word = word.strip(".'?,!\"_-:;")
-        if word in word_count:
-            word_count[word] += 1
+        word = word.upper()
+        word = word.strip(".'?,!\"_-:;*()")
+
+        # split words more - separate those containing --
+        if "--" in word:
+            more_words = word.split("--")
+            for word in more_words:
+                word = word.strip(".'?,!\"_-:;*()")
+                if word in word_count:
+                    word_count[word] += 1
+                else:
+                    word_count[word] = 1
         else:
-            word_count[word] = 1
+            if word in word_count:
+                word_count[word] += 1
+            else:
+                word_count[word] = 1
 
-# print key value pairs
-for word, count in word_count.iteritems():
-    print "%s: %d" % (word, count)
+# get items (count values) from word_count
+item_list = word_count.values()
 
+# remove duplicates & sort from highest to lowest
+item_list = list(set(item_list))
+item_list.sort()
+item_list.reverse()
+
+# iterate
+for value in item_list:
+    key_list_for_value = []
+    print "***********************"
+    print "Words that occur %d times:" % value
+    # print key/value pairs with that value
+    for word, count in word_count.iteritems():
+        if count == value:
+            key_list_for_value.append(word)
+    key_list_for_value.sort()
+    
+    # print formatted list of words for this value
+    for word in key_list_for_value: 
+        print word 
