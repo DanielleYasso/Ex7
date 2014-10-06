40edc74a2580949bb19228e3c00d74735119be25
Hackbright Student <info@hackbrightacademy.com>
2014-10-06 12:54:33 -0700

    First draft of ex 07

---
+++ wordcount.py
@@ -0,0 +1,20 @@
+# open file
+filename = open("twain.txt")
+
+# create empty dictionary to hold words and counts
+word_count = {}
+
+# loop through file reading lines and separate lines into words
+for line in filename: 
+    words =line.rstrip().split()
+
+    # check if words are already in dictionary, increment by 1
+    for word in words:
+        if word in word_count:
+            word_count[word] += 1
+        else:
+            word_count[word] = 1
+
+# print key value pairs
+for word, count in word_count.iteritems():
+    print "%s: %d" % (word, count)
